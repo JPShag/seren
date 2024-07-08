64911dc508a0c0be00af0a41e6d1146846ef21f7
vcpu <xuofmx@gmail.com>
2024-07-08 19:14:34 -0400

    chore: Refactor provider_tools.py for improved readability and maintainability

--- resources/lib/common/maintenance.py
+++ resources/lib/common/maintenance.py
@@ -8,56 +8,42 @@ from resources.lib.common import tools
 from resources.lib.database import cache
 from resources.lib.database.premiumizeTransfers import PremiumizeTransfers
 from resources.lib.database.skinManager import SkinManager
-from resources.lib.debrid import all_debrid
-from resources.lib.debrid import premiumize
-from resources.lib.debrid import real_debrid
+from resources.lib.debrid import all_debrid, premiumize, real_debrid
 from resources.lib.indexers.trakt import TraktAPI
 from resources.lib.indexers.tvdb import TVDBAPI
 from resources.lib.modules.globals import g
 from resources.lib.modules.providers.install_manager import ProviderInstallManager
 
-
 def update_themes():
     """
-    Performn checks for any theme updates
-    :return: None
-    :rtype: None
+    Perform checks for any theme updates.
     """
     if g.get_bool_setting("skin.updateAutomatic"):
         SkinManager().check_for_updates(silent=True)
 
-
 def update_provider_packages():
     """
-    Perform checks for provider package updates
-    :return: None
-    :rtype: None
+    Perform checks for provider package updates.
     """
     provider_check_stamp = g.get_float_runtime_setting("provider.updateCheckTimeStamp", 0)
     automatic = g.get_bool_setting("providers.autoupdates")
-    if time.time() > (provider_check_stamp + (24 * (60 * 60))):
+    if time.time() > (provider_check_stamp + 24 * 60 * 60):
         available_updates = ProviderInstallManager().check_for_updates(silent=True, automatic=automatic)
-        if not automatic and len(available_updates) > 0:
+        if not automatic and available_updates:
             g.notification(g.ADDON_NAME, g.get_language_string(30253))
         g.set_runtime_setting("provider.updateCheckTimeStamp", str(time.time()))
 
-
 def refresh_apis():
     """
-    Refresh common API tokens
-    :return: None
-    :rtype: None
+    Refresh common API tokens.
     """
     TraktAPI().try_refresh_token()
     real_debrid.RealDebrid().try_refresh_token()
     TVDBAPI().try_refresh_token()
 
-
 def wipe_install():
     """
-    Destroys Seren's user_data folder for current user resetting addon to default
-    :return: None
-    :rtype: None
+    Destroys Seren's user_data folder for current user resetting addon to default.
     """
     confirm = xbmcgui.Dialog().yesno(g.ADDON_NAME, g.get_language_string(30083))
     if confirm == 0:
@@ -75,12 +61,9 @@ def wipe_install():
         xbmcvfs.rmdir(path, True)
     xbmcvfs.mkdir(g.ADDON_USERDATA_PATH)
 
-
 def premiumize_transfer_cleanup():
     """
-    Cleanup transfers created by Seren at Premiumize
-    :return: None
-    :rtype: NOne
+    Cleanup transfers created by Seren at Premiumize.
     """
     service = premiumize.Premiumize()
     premiumize_transfers = PremiumizeTransfers()
@@ -90,45 +73,34 @@ def premiumize_transfer_cleanup():
     if fair_usage < threshold:
         g.log("Premiumize Fair Usage below threshold, no cleanup required")
         return
+
     seren_transfers = premiumize_transfers.get_premiumize_transfers()
     if seren_transfers is None:
         g.log("Failed to cleanup transfers, API error", "error")
         return
-    if len(seren_transfers) == 0:
+
+    if not seren_transfers:
         g.log("No Premiumize transfers have been created")
         return
-    g.log("Premiumize Fair Usage is above threshold, cleaning up Seren transfers")
-    for i in seren_transfers:
-        service.delete_transfer(i["transfer_id"])
-        premiumize_transfers.remove_premiumize_transfer(i["transfer_id"])
 
+    g.log("Premiumize Fair Usage is above threshold, cleaning up Seren transfers")
+    for transfer in seren_transfers:
+        service.delete_transfer(transfer["transfer_id"])
+        premiumize_transfers.remove_premiumize_transfer(transfer["transfer_id"])
 
 def account_premium_status_checks():
     """
-    Updates premium status settings to reflect current state and advises users of expiries if enabled
-    :return: None
-    :rtype: None
+    Updates premium status settings to reflect current state and advises users of expiries if enabled.
     """
-
     def set_settings_status(debrid_provider, status):
         """
-        Ease of use method to set premium status setting
-        :param debrid_provider: setting prefix for debrid provider
-        :type debrid_provider: str
-        :param is_premium: Status of premium status
-        :type is_premium: bool
-        :return: None
-        :rtype: None
+        Ease of use method to set premium status setting.
         """
         g.set_setting(f"{debrid_provider}.premiumstatus", status.title())
 
     def display_expiry_notification(display_debrid_name):
         """
-        Ease of use method to notify user of expiry of debrid premium status
-        :param display_debrid_name: Debrid providers full display name
-        :type display_debrid_name: str
-        :return: None
-        :rtype: None
+        Notify user of expiry of debrid premium status.
         """
         if g.get_bool_setting("general.accountNotifications"):
             g.notification(
@@ -142,17 +114,19 @@ def account_premium_status_checks():
         ("All Debrid", all_debrid.AllDebrid, "alldebrid"),
     ]
 
-    for service in valid_debrid_providers:
-        service_module = service[1]()
+    for name, service_module_class, prefix in valid_debrid_providers:
+        service_module = service_module_class()
         if service_module.is_service_enabled():
             status = service_module.get_account_status()
             if status == "expired":
-                display_expiry_notification(service[0])
-            g.log(f"{service[0]}: {status}")
-            set_settings_status(service[2], status)
-
+                display_expiry_notification(name)
+            g.log(f"{name}: {status}")
+            set_settings_status(prefix, status)
 
 def toggle_reuselanguageinvoker(forced_state=None):
+    """
+    Toggle the state of reuselanguageinvoker setting in addon.xml and reload the profile.
+    """
     def _store_and_reload(output):
         with open(file_path, "w+") as addon_xml:
             addon_xml.writelines(output)
@@ -164,66 +138,38 @@ def toggle_reuselanguageinvoker(forced_state=None):
     with open(file_path) as addon_xml:
         file_lines = addon_xml.readlines()
 
-    for i in range(len(file_lines)):
-        line_string = file_lines[i]
-        if "reuselanguageinvoker" in file_lines[i]:
-            if ("false" in line_string and forced_state is None) or ("false" in line_string and forced_state):
-                file_lines[i] = file_lines[i].replace("false", "true")
+    for i, line in enumerate(file_lines):
+        if "reuselanguageinvoker" in line:
+            if ("false" in line and forced_state is None) or ("false" in line and forced_state):
+                file_lines[i] = line.replace("false", "true")
                 g.set_setting("reuselanguageinvoker.status", "Enabled")
                 _store_and_reload(file_lines)
-            elif ("true" in line_string and forced_state is None) or ("true" in line_string and forced_state is False):
-                file_lines[i] = file_lines[i].replace("true", "false")
+            elif ("true" in line and forced_state is None) or ("true" in line and forced_state is False):
+                file_lines[i] = line.replace("true", "false")
                 g.set_setting("reuselanguageinvoker.status", "Disabled")
                 _store_and_reload(file_lines)
             break
 
-
-# def clean_deprecated_settings():
-#     """
-#     Removes settings no longer defined in the settings.xml file from the users user_data settings file
-#     :return: None
-#     :rtype: None
-#     """
-#     settings_helper = SettingsHelper()
-#     settings_helper.create_and_clean_settings()
-#     if len(settings_helper.valid_settings) != len(
-#         settings_helper.current_user_settings
-#     ):
-#         g.log(
-#             "Mismatch in valid settings, cancelling the removal of deprecated settings",
-#             "warning",
-#         )
-#         return
-#     if len(settings_helper.removed_settings) == 0:
-#         return
-#     settings_helper.save_settings()
-#     g.log(
-#         "Filtered settings, removed {} deprecated settings".format(
-#             len(settings_helper.removed_settings)
-#         )
-#     )
-
-
 def run_maintenance():
     """
-    Entry point for background maintenance cycle
-    :return: None
-    :rtype: None
+    Entry point for background maintenance cycle.
     """
     g.log("Performing Maintenance")
     # ADD COMMON HOUSE KEEPING ITEMS HERE #
 
     # Refresh API tokens
-
     try:
         refresh_apis()
     except Exception as e:
         g.log(f"Failed to update API keys: {e}", 'error')
 
+    # Check account premium statuses
     try:
         account_premium_status_checks()
     except Exception as e:
         g.log(f"Failed to check account status: {e}", 'error')
+
+    # Update provider packages and themes
     ProviderInstallManager()
     update_provider_packages()
     update_themes()
@@ -235,5 +181,5 @@ def run_maintenance():
         except Exception as e:
             g.log(f"Failed to cleanup PM transfers: {e}", 'error')
 
-    # clean_deprecated_settings()
+    # Cache cleanup
     cache.Cache().check_cleanup()
--- resources/lib/common/provider_tools.py
+++ resources/lib/common/provider_tools.py
@@ -1,29 +1,31 @@
 from resources.lib.modules.providers.settings import SettingsManager
 
-
 def get_setting(package_name, setting_id):
     """
-    Retrieves a setting value for a provider package
-    :param package_name: name of package
+    Retrieves a setting value for a provider package.
+    
+    :param package_name: The name of the package.
     :type package_name: str
-    :param setting_id: ID of the setting to retrieve
+    :param setting_id: The ID of the setting to retrieve.
     :type setting_id: str
-    :return: Value of settings
+    :return: The value of the specified setting.
     :rtype: object
     """
-    return SettingsManager().get_setting(package_name, setting_id)
-
+    settings_manager = SettingsManager()
+    return settings_manager.get_setting(package_name, setting_id)
 
 def set_setting(package_name, setting_id, value):
     """
-    Sets the value of a setting for a provider package
-    :param package_name: name of package
+    Sets the value of a setting for a provider package.
+    
+    :param package_name: The name of the package.
     :type package_name: str
-    :param setting_id: ID of the setting to set
+    :param setting_id: The ID of the setting to set.
     :type setting_id: str
-    :param value: New value for setting
-    :rtype value: object
-    :return: Value of settings
+    :param value: The new value for the setting.
+    :type value: object
+    :return: The value of the setting after it is set.
     :rtype: object
     """
-    SettingsManager().set_setting(package_name, setting_id, value)
+    settings_manager = SettingsManager()
+    return settings_manager.set_setting(package_name, setting_id, value)
